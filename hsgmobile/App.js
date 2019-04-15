562405ce16c10895fc065c7e74b7fd3e12b90fd0
Tom Le <lmt1205@uw.edu>
2019-04-15 13:15:05 -0700

    Login Signup Flow

--- hsgmobile/App.js
+++ hsgmobile/App.js
@@ -1,48 +1,31 @@
-import React from 'react';
-import { Icon } from 'native-base';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
-import HomeScreen from './src/components/home/HomeScreen';
-import HostScreen from './src/components/host/HostScreen';
-import NotiScreen from './src/components/noti/NotiScreen';
+import React from "react";
+import { createRootNavigator } from "./src/components/routes/router";
+import { isSignedIn } from "./auth";
 
-const NavStack = createBottomTabNavigator(
-	{
-		// Test: SearchResultScreen, // Test screen for UI developing, comment out when doing UI test.
-		Home: HomeScreen,
-		Host: HostScreen,
-		Noti: NotiScreen
-	},
-	{
-		defaultNavigationOptions: ({ navigation }) => ({
-			tabBarIcon: ({ tintColor }) => {
-				// console.log(navigation.state)
-				const { routeName } = navigation.state;
-				let iconName;
-				if (routeName === 'Home') {
-					iconName = `md-home`;
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
 
-				// Sometimes we want to add badges to some icons. 
-				// You can check the implementation below.
-				// IconComponent = HomeIconWithBadge; 
-				} else if (routeName === 'Host') {
-					iconName = `ios-basketball`;
-				} else if (routeName === 'Noti') {
-					iconName = 'ios-notifications';
-				} else if (routeName === 'Test') {
-					iconName = 'ios-settings';
-				}
-
-				// You can return any component that you like here!
-				return <Icon name={iconName} size={25} color={tintColor} />;
-			},
-		}),
-		tabBarOptions: {
-			activeTintColor: '#000000',
-			inactiveTintColor: '#FFFFFC',
-			activeBackgroundColor: '#FECC52',
-			inactiveBackgroundColor: '#F49F0A'
-		},
+		this.state = {
+			signedIn: false,
+			checkedSignIn: false
+		};
 	}
-);
 
-export default createAppContainer(NavStack);
+	componentDidMount() {
+		isSignedIn()
+			.then(res => this.setState({ signedIn: res, checkedSignIn: true }));
+	}
+
+	render() {
+		const { checkedSignIn, signedIn } = this.state;
+
+		// If we haven't checked AsyncStorage yet, don't render anything (better ways to do this)
+		if (!checkedSignIn) {
+			return null;
+		}
+
+		const Layout = createRootNavigator(signedIn);
+		return <Layout />;
+	}
+}
